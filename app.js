004bd3e31fe438aff164a26a1b9f30acbcc68b4b
daniel.mlaszlo@yahoo.com <daniel.mlaszlo@yahoo.com>
2023-04-15 13:21:08 +0200

    fixing bugs

--- app.js
+++ app.js
@@ -8,9 +8,9 @@ const xss = require('xss-clean')
 const rateLimiter = require('express-rate-limit')
 
 // Swagger
-// const swaggerUI = require('swagger-ui-express');
-// const YAML = require('yamljs');
-// const swaggerDocument = YAML.load('./swagger.yaml');
+const swaggerUI = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./swagger.yaml');
 
 
 const express = require('express')
@@ -37,10 +37,10 @@ app.use(helmet())
 app.use(cors())
 app.use(xss())
 
-// app.get('/', (req, res) => {
-//     res.send('<h1>Jobs API</h1><a href="/api-docs">Documentation</a>');
-//   });
-//   app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
+app.get('/', (req, res) => {
+    res.send('<h1>Jobs API</h1><a href="/api-docs">Documentation</a>');
+  });
+  app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 // routes
 app.use('/api/v1/auth', authRouter)
